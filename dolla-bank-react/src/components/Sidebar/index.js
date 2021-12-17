810105b27cc8c280bd94eef1ea5d6c005bbbb3cd
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-12-17 15:46:47 -0300

    Sidebar Route

--- dolla-bank-react/src/components/Sidebar/index.js
+++ dolla-bank-react/src/components/Sidebar/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { 
 SidebarContainer,
 Icon,
-CloseIcon
+CloseIcon,
+SidebarWrapper,
+SidebarMenu,
+SidebarLink,
+SideBtnWrap,
+SidebarRoute
 } from './SidebarElements';
 
 const Sidebar = () => {
